a4530410b1510d0dfe78c2324daa4f94323cfb34
Sherwood Callaway <shcallaway@gmail.com>
2018-10-02 07:42:55 -0700

    Use default value for COUNT, print pageId after request comes back

--- scripts/download_anime_pages.sh
+++ scripts/download_anime_pages.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 API="https://bj1475vt9h.execute-api.us-east-1.amazonaws.com/default/downloadAnimePages"
-COUNTER=0
+COUNT=${COUNT:-0}
 
-while [  $COUNTER -lt 500 ]; do
-    echo $COUNTER
-    curl -H "x-api-key: $API_KEY" -H "content-type: application/json" -d "{ \"pageId\": \"$COUNTER\" }" $API
-    let COUNTER=COUNTER+1 
+while [  $COUNT -lt 500 ]; do
+    curl -H "x-api-key: $API_KEY" -H "content-type: application/json" -d "{ \"pageId\": \"$COUNT\" }" $API
+    echo "pageId=$COUNT"
+    let COUNT=COUNT+1
     sleep 60
 done
