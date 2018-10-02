8f7a8a0a4d3232cb3f14d0eea4aa1462a732c735
Sherwood Callaway <shcallaway@gmail.com>
2018-10-02 09:33:59 -0700

    Respond with 500 when put_object fails

--- lambda/download_anime_pages.py
+++ lambda/download_anime_pages.py
@@ -3,7 +3,7 @@ import urllib.request
 import os
 import json
 
-BUCKET_NAME = os.environ["BUCKET_NAME"]
+bucket = os.environ["BUCKET_NAME"]
 
 
 def request(url):
@@ -16,17 +16,12 @@ def request(url):
         return None
 
 
-def upload(bucket, key, body):
-    client = boto3.client("s3")
+def respond_with(status_code):
+    print(f"Response Status Code: {status_code}")
 
-    try:
-        client.put_object(
-            Bucket=bucket,
-            Key=key,
-            Body=body
-        )
-    except Exception as error:
-        print(f"Error: {error}")
+    return {
+        "statusCode": status_code
+    }
 
 
 def handler(event, context):
@@ -38,9 +33,7 @@ def handler(event, context):
         id = json.loads(event["body"])["pageId"]
     except Exception as error:
         print(f"Error: {error}")
-        return {
-            "statusCode": 400
-        }
+        return respond_with(400)
 
     url = f"https://myanimelist.net/anime/{id}"
     key = f"anime/{id}.html"
@@ -48,12 +41,18 @@ def handler(event, context):
     data = request(url)
 
     if data:
-        upload(BUCKET_NAME, key, data)
+        client = boto3.client("s3")
 
-        return {
-            "statusCode": 201
-        }
+        try:
+            client.put_object(
+                Bucket=bucket,
+                Key=key,
+                Body=data
+            )
+        except Exception as error:
+            print(f"Error: {error}")
+            return respond_with(500)
 
-    return {
-        "statusCode": 200
-    }
+        return respond_with(201)
+
+    return respond_with(200)
