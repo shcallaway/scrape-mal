861d05c9c279e0ec4628a2c7eac0f6b1dc0b6b1e
Sherwood Callaway <shcallaway@gmail.com>
2018-10-13 21:52:30 -0700

    Rename insert_database_record lambda function

--- lambda/insert_anime_record.py
+++ lambda/insert_anime_record.py
@@ -1,3 +1,8 @@
+# import boto3
+
+
 def handler(event, context):
+    print(f"Event: {event}")
     # This lambda should accept a JSON payload with data from
     # a scraped page and insert it into the database
+    # client = boto3.client('rds')
