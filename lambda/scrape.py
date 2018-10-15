58d381cbe160a2ceb61e076ab0ea0c0af33b95c2
Sherwood Callaway <shcallaway@gmail.com>
2018-10-14 18:31:47 -0700

    Create scrape lambda that combines extraction and insertion

---
+++ lambda/scrape.py
@@ -0,0 +1,34 @@
+import json
+import os
+import botocore.vendored.requests as requests
+
+
+def lambda_handler(event, context):
+    mal_id = None
+
+    try:
+        mal_id = json.loads(event["body"])["mal_id"]
+    except Exception as error:
+        print(error)
+        return {"statusCode": 400}
+
+    url = os.environ["URL"]
+
+    extract_api_key = os.environ["EXTRACT_API_KEY"]
+    insert_api_key = os.environ["INSERT_API_KEY"]
+
+    response = requests.post(
+        f"{url}/default/extract",
+        data=json.dumps({'mal_id': mal_id}),
+        headers={'x-api-key': extract_api_key}
+    )
+
+    data = response.json()
+
+    response = requests.post(
+        f"{url}/default/insertAnimeRecord",
+        data=json.dumps(data),
+        headers={'x-api-key': insert_api_key}
+    )
+
+    return {"statusCode": 201}
