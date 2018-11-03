c1fcb120649e1c970939628411c682228beda362
Sherwood Callaway <shcallaway@gmail.com>
2018-11-03 11:26:53 -0700

    This deployment package works for extract.js

--- scripts/create_deployment_package.sh
+++ scripts/create_deployment_package.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-DIR="tmp"
-rm -rf $DIR
-cp -R lambda $DIR
+DIRECTORY="tmp"
+ZIPFILE="scrape_mal.zip"
+
+rm -rf $ZIPFILE $DIRECTORY
+mkdir $DIRECTORY
+
+cp -R lambda/extract.js $DIRECTORY/extract.js
+
+chmod u=rwx,g=rx,o=rx tmp/*.js
+chmod u=rwx,g=rx,o=rx tmp/*.py
 
 # psycopg2
-git clone https://github.com/jkehler/awslambda-psycopg2.git $DIR/awslambda-psycopg2
-mv $DIR/awslambda-psycopg2/psycopg2-3.6 $DIR/psycopg2
-rm -rf $DIR/awslambda-psycopg2
+# git clone https://github.com/jkehler/awslambda-psycopg2.git $DIRECTORY/awslambda-psycopg2
+# mv $DIRECTORY/awslambda-psycopg2/psycopg2-3.6 $DIRECTORY/psycopg2
+# rm -rf $DIRECTORY/awslambda-psycopg2
 
-./venv/bin/pip3 install bs4 -t $DIR
-npm i --prefix=$DIR cheerio aws-sdk
+# ./venv/bin/pip3 install bs4 -t $DIRECTORY
+npm i --prefix=$DIRECTORY cheerio aws-sdk
+# chmod 777 $DIRECTORY/node_modules
 
-zip -r scrape_mal.zip $DIR/*
-rm -rf $DIR
+cd $DIRECTORY
+zip -r ../$ZIPFILE *
+cd ..
+
+rm -rf $DIRECTORY
